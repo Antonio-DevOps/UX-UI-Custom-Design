923ad385659dbf921c3de40d5f08aa4cd7fffc51
ksion4ik1 <ksion4ik.life@gmail.com>
2020-03-23 05:00:11 +0200

    add media screens for Ipad

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $( document ).ready()
 
 $('.fade').slick({
-    dots: true,
+    dots: false,
     infinite: true,
     speed: 500,
     fade: true,
@@ -9,30 +9,96 @@ $('.fade').slick({
     autoplay: true,
     autoplaySpeed: 2000,
   });
+  //=======================================accordion_menu===============================================
+  $(function() {
+    let Accordion = function(el, multiple) {
+      this.el = el || {};
+      // more then one submenu open?
+      this.multiple = multiple || false;
+      
+      let dropdownlink = this.el.find('.dropdownlink');
+      dropdownlink.on('click',
+                      { el: this.el, multiple: this.multiple },
+                      this.dropdown);
+    };
+    
+    Accordion.prototype.dropdown = function(e) {
+      let $el = e.data.el,
+          $this = $(this),
+          //this is the ul.submenuItems
+          $next = $this.next();
+      
+      $next.slideToggle();
+      $this.parent().toggleClass('open');
+      
+      if(!e.data.multiple) {
+        //show only one menu at the same time
+        $el.find('.submenuItems').not($next).slideUp().parent().removeClass('open');
+      }
+    }
+    
+    let accordion = new Accordion($('.accordion-menu'), false);
+  })
   //============================================///================Burger==========================///================
-
-function burgerMenu(selector){
-  let menu = $(selector);
-  let button = menu.find(".burger-menu_button");
-  let links = menu.find(".burger_menu__link");
-  let overlay = menu.find (".burger_menu__overlay");
+  function burgerMenu(selector){
+    let menu = $(selector);
+    let button = menu.find(".burger-menu_button");
+    let links = menu.find(".burger_menu__link");
+    let overlay = menu.find (".burger-menu_overlay");
+    
+    button.on("click", (e)=> {
+      e.preventDefault();
+      toggleMenu();
+    });
   
-  button.on("click", (e)=> {
-    e.preventDefault();
-    toggleMenu();
-  });
-
-    links.on("click", () => toggleMenu());
-    overlay.on("click", () => toggleMenu());
-
-  function toggleMenu(){
-    menu.toggleClass("burger-menu_active");
-
-    if (menu.hasClass("burger-menu_active")) {
-      $("body").css("overflow", "hidden");
-    } else{
-      $("body").css("overflow", "visible");
+      links.on("click", () => toggleMenu());
+      overlay.on("click", () => toggleMenu());
+  
+    function toggleMenu(){
+      menu.toggleClass("burger-menu_active");
+  
+      if (menu.hasClass("burger-menu_active")) {
+        $("body").css("overflow", "hidden");
+      } else{
+        $("body").css("overflow", "visible");
+      }
     }
   }
-}
-burgerMenu(".burger-menu");
+  burgerMenu(".burger-menu");
+   // ==============================================================Scroll_nav===============================
+   $(window).scroll(function () {
+		if ($(window).scrollTop() >= 1024) {
+			$(".burger-menu").css({
+				'display': 'none'
+			});	
+		}
+		else 
+		{
+		$(".burger-menu").css({
+			'display': 'block'
+		});
+		};
+  });
+  // -------------------------For ScrollArrow_nav-------------------------------------------
+  let allArrow = document.querySelectorAll(".arrowGen");
+  for (let i = 0; i < allArrow.length; i++) {
+    allArrow[i].onclick = function (event) {
+      event.preventDefault();
+      let blockID = this.getAttribute('href');
+      document.querySelector('' + blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    }
+  }
+
+  for (let i = 0; i < allArrow.length; i++) {
+    allArrow[i].ontouchstart = function (event) {
+      event.preventDefault();
+      let blockID = this.getAttribute('href');
+      document.querySelector('' + blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    }
+  }
